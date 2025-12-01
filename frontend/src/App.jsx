ff8dae975eae2fc4b91267f6807795b4d85b75d0
Aditya-Thakor <aditya.github.27x@gmail.com>
2025-12-01 15:54:08 +0530

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+       <h1 className='text-5xl text-blue-500 text-center mt-10 font-bold'>Expense Tracker</h1>
+    </>
+  )
+}
+
+export default App
