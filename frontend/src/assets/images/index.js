6f9f5d1d298596a39a9811520d44149312c8fb71
Aditya-Thakor <aditya.github.27x@gmail.com>
2025-12-01 17:33:30 +0530

    add profile images

---
+++ frontend/src/assets/images/index.js
@@ -0,0 +1,18 @@
+// profile images
+import pfp1 from "./pfp/pfp1.svg";
+import pfp2 from "./pfp/pfp2.svg";
+import pfp3 from "./pfp/pfp3.svg";
+import pfp4 from "./pfp/pfp4.svg";
+import pfp5 from "./pfp/pfp5.svg";
+import pfp6 from "./pfp/pfp6.svg";
+import pfp7 from "./pfp/pfp7.svg";
+
+export default {
+  pfp1,
+  pfp2,
+  pfp3,
+  pfp4,
+  pfp5,
+  pfp6,
+  pfp7,
+};
