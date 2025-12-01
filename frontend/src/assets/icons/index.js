0c72072174c304e33e96df4b7691a7bc0d9c6332
Aditya-Thakor <aditya.github.27x@gmail.com>
2025-12-01 17:18:54 +0530

    add icons

---
+++ frontend/src/assets/icons/index.js
@@ -0,0 +1,52 @@
+//logos
+import logo from "./logo/Group 335.svg";
+
+//navigation icons
+import category from "./category.svg";
+import chart from "./charts.svg";
+import dashboard from "./dashboard.svg";
+import expense from "./expense.svg";
+import income from "./income.svg";
+import logout from "./logout.svg";
+import plus from "./plus.svg";
+import transaction from "./transaction.svg";
+import user from "./user.svg";
+
+// category icons
+import bill from "./categories/bills.svg";
+import education from "./categories/education.svg.svg";
+import entertainment from "./categories/entertainment.svg";
+import food from "./categories/food.svg";
+import health from "./categories/health.svg";
+import shopping from "./categories/shopping.svg";
+import transportation from "./categories/transportation.svg";
+import travel from "./categories/travel.svg";
+import code from "./categories/code.svg";
+import office from "./categories/office.svg";
+import dining from "./categories/dining.svg";
+import saving from "./saving.svg";
+
+export default {
+  logo,
+  category,
+  chart,
+  dashboard,
+  expense,
+  income,
+  logout,
+  plus,
+  transaction,
+  user,
+  bill,
+  education,
+  entertainment,
+  food,
+  health,
+  shopping,
+  transportation,
+  travel,
+  code,
+  dining,
+  office, 
+  saving,
+};
