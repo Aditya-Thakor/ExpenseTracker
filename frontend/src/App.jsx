ad24481be9edd0743f729663bf5911afe9365e7a
Aditya-Thakor <aditya.github.27x@gmail.com>
2025-12-01 17:18:25 +0530

    add fonts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-       <h1 className='text-5xl text-blue-500 text-center mt-10 font-bold'>Expense Tracker</h1>
+       <h1 className='text-5xl text-blue-500 text-center mt-10 font-lato '>Expense Tracker</h1>
     </>
   )
 }
