151a3b2490da78f58cb63049ff27133d48ac41cd
Aditya-Thakor <aditya.github.27x@gmail.com>
2025-12-13 18:24:06 +0530

    add transaction FN

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const  cors =require ('cors');
-const connectDB = require('./db/index');
-const multer = require('multer')
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db/index");
+const multer = require("multer");
+const { ObjectId } = require("mongodb");
 const app = express();
 const upload = multer();
 const port = 5000;
@@ -9,38 +10,75 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send('Server is ready')
-
-})
-
-app.post('/usersdata',upload.none(), async (req,res)=>{
-    console.log('user info request');
-
-   const data= req.body;
-
-    const db = await connectDB();
-
-    const userData = await db.collection('usersdata').insertOne({
-        username: data.username,
-        email:data.email,
-        password:data.password
-    });
-    console.log(userData);
-    res.send('user added successfully!!')
-    res.json(userData)
-    res.end();
-})
-
-app.get('/usersdata', async(req,res)=>{
-    const db = await connectDB();
-    const data =  await db.collection('userdata').find().toArray();
-
-    res.json(data)
-    res.end();
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
 
+app.post("/usersdata", upload.none(), async (req, res) => {
+  console.log("user info request");
 
-app.listen(port,()=>{
-    console.log(`Server started at ${port} PORT.`);
-})
+  const data = req.body;
+
+  const db = await connectDB();
+
+  const userData = await db.collection("usersdata").insertOne({
+    username: data.username,
+    email: data.email,
+    password: data.password,
+  });
+  console.log(userData);
+  res.send("user added successfully!!");
+  res.json(userData);
+  res.end();
+});
+
+app.get("/usersdata", async (req, res) => {
+  const db = await connectDB();
+  const data = await db.collection("usersdata").find().toArray();
+
+  res.json(data);
+  res.end();
+});
+
+// add transactions
+app.post("/usersdata/transactions", upload.none(), async (req, res) => {
+  // const data = req.body;
+  console.log(req.body);
+  const { type, amount, description, incomeFrom, category, date } = req.body;
+
+  try {
+    const db = await connectDB();
+    const transaction = {
+      _id: new ObjectId(),
+      type,
+      amount: Number(amount),
+      description,
+      incomeFrom,
+      category,
+      date: new Date(date),
+    };
+    const update = await db.collection("usersdata").updateOne(
+      { _id: new ObjectId("693c0c7f7badfd7ae01a78be") },
+      {
+        $push: { transactions: transaction },
+      }
+    );
+
+    res.send("transaction added successfully!!");
+  } catch (error) {
+    console.log("Error at adding transaction", error);
+    throw error;
+  }
+});
+
+app.get("/usersdata/transactions", async (req, res) => {
+  const db = await connectDB();
+  const data = await db.collection("usersdata").find().toArray();
+
+  res.json(data);
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`Server started at ${port} PORT.`);
+});
--- frontend/src/components/addTransactions/modelDemoz/D1.jsx
+++ frontend/src/components/addTransactions/modelDemoz/D1.jsx
@@ -1,13 +1,41 @@
 import { IndianRupee } from "lucide-react";
 import React, { useState } from "react";
 
-export default function AddTransactionModal({open,onClose}) {
+export default function AddTransactionModal({ open, onClose }) {
   // const [open, setOpen] = useState(false);
   const [step, setStep] = useState(1);
   const [type, setType] = useState("income");
 
+  const [amount, setAmount]= useState(0);
+  const [description, setDescription]= useState('');
+  const [incomeFrom,setIncomeFrom]=useState('');
+  const [category, setCategory]= useState('');
+  const [date,setDate]= useState('');
+
+  const handleTransaction = async()=>{
+    const formData = new FormData();
+
+    formData.append("type", type);
+    formData.append("amount", amount);
+    formData.append("description", description);
+    formData.append("incomeFrom", incomeFrom);
+    formData.append("category", category);
+    formData.append("date",date);
+
+    for (const [key,value] of formData) {
+      console.log(key,value);      
+    }
+
+    const addTransaction = await fetch('http://localhost:5000/usersdata/transactions',{
+      method:'post',
+      body:formData
+    });
+    console.log(await addTransaction.text());
+    
+
+  }
   const closeModal = () => {
-    onClose()
+    onClose();
     // setOpen(false);
     setStep(1);
     setType("income");
@@ -16,82 +44,14 @@ export default function AddTransactionModal({open,onClose}) {
 
   return (
     <div>
-      {/* <button
-        onClick={() => closeModal}
-        className="rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
-      >
-        Add Transaction
-      </button> */}
-        {/* if open=true && step=1 ? add amount model else details model */}
-      
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
-          {step === 1 && (
-            <div className="h-3/4 w-full max-w-sm flex flex-col justify-between rounded-xl bg-white p-6 shadow-lg">
-              <div className="mb-4 flex flex-col gap-5 w-full items-start justify-between">
-                <div className="w-full flex">
-                  <div className="w-full">
-                    <h2 className="text-lg font-semibold">Add Transaction</h2> 
-                  </div>
-                  <button
-                    onClick={closeModal}
-                    className="text-gray-400 hover:text-gray-600"
-                  >
-                    ✕
-                  </button>
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
+        {step === 1 && (
+          <div className="h-3/4 w-full max-w-sm flex flex-col justify-between rounded-xl bg-white p-6 shadow-lg">
+            <div className="mb-4 flex flex-col gap-5 w-full items-start justify-between">
+              <div className="w-full flex">
+                <div className="w-full">
+                  <h2 className="text-lg font-semibold">Add Transaction</h2>
                 </div>
-
-                <div className="mb-6 w-full flex justify-between gap-3">
-                  <button
-                    onClick={() => setType("income")}
-                    className={`flex-1 rounded-lg border px-4 py-2 font-medium transition ${
-                      type === "income"
-                        ? "border-green-500 bg-green-600 text-white"
-                        : "border-gray-300 text-gray-600 hover:bg-gray-100"
-                    }`}
-                  >
-                    Income
-                  </button>
-                  <button
-                    onClick={() => setType("expense")}
-                    className={`flex-1 rounded-lg border px-4 py-2 font-medium transition ${
-                      type === "expense"
-                        ? "border-red-500 bg-red-600 text-white"
-                        : "border-gray-300 text-gray-600 hover:bg-gray-100"
-                    }`}
-                  >
-                    Expense
-                  </button>
-                </div>
-              </div>
-
-            {/* add amount */}
-              <div className="mb-6 flex items-center justify-center relative">
-                <IndianRupee className="size-9 text-gray-700 absolute left-1/4 pl-2" />
-                <input
-                  type="number"
-                  placeholder="0.00"
-                  className="w-40 h-9 pl-10  border rounded-lg  pr-0 py-7 text-start tracking-wide text-3xl text-blue-600 focus:border-blue-600 focus:outline-none"
-                />
-              </div>
-
-              {/* next btn */}
-              <button
-                onClick={() => setStep(2)}
-                className="w-full rounded-lg bg-blue-600 py-2 text-white hover:bg-blue-700"
-              >
-                Next
-              </button>
-            </div>
-          )}
-
-          {/* details model */}
-          {step === 2 && (
-            <div className="h-3/4 w-full max-w-sm flex flex-col justify-between rounded-xl bg-white p-6 shadow-lg">
-             
-              <div className="mb-4 flex items-start justify-between">
-                <h2 className="text-lg font-semibold">
-                  {type === "expense" ? "Add Expense" : "Add Income"}
-                </h2>
                 <button
                   onClick={closeModal}
                   className="text-gray-400 hover:text-gray-600"
@@ -100,62 +60,134 @@ export default function AddTransactionModal({open,onClose}) {
                 </button>
               </div>
 
-              {/* Form */}
-              <div className="space-y-4">
-                <input
-                  type="text"
-                  placeholder="Description"
-                  className="w-full rounded-lg border px-4 py-2 focus:border-blue-600 focus:outline-none"
-                />
-
-                {type === "expense" ? (
-                  <select className="w-full overflow-hidden overflow-y-scroll rounded-lg border px-4 py-2 focus:border-blue-600 focus:outline-none">
-                    <option value="">Select Category</option>
-                    <option>Food</option>
-                    <option>Bills & utilities</option>
-                    <option>travel</option>
-                    <option>Transport</option>
-                    <option>Healthcare</option>
-                    <option>Shopping</option>
-                    <option>Education</option>
-                    <option>Entertainment</option>
-                  </select>
-                ) : (
-                  <input
-                    type="text"
-                    placeholder="Income From"
-                    className="w-full rounded-lg border px-4 py-2 focus:border-blue-600 focus:outline-none"
-                  />
-                )}
-
-                <input
-                  type="date"
-                  className="w-full rounded-lg border px-4 py-2 focus:border-blue-600 focus:outline-none"
-                />
-              </div>
-
-              {/* btns */}
-              <div className="mt-6 flex gap-3">
+              <div className="mb-6 w-full flex justify-between gap-3">
                 <button
-                  onClick={() => setStep(1)}
-                  className="flex-1 rounded-lg border py-2 text-gray-600 hover:bg-gray-100"
-                >
-                  Back
-                </button>
-                <button
-                  className={`flex-1 rounded-lg py-2 text-white ${
-                    type === "expense"
-                      ? "bg-red-600 hover:bg-red-700"
-                      : "bg-green-600 hover:bg-green-700"
+                  onClick={() => setType("income")}
+                  className={`flex-1 rounded-lg border px-4 py-2 font-medium transition ${
+                    type === "income"
+                      ? "border-green-500 bg-green-600 text-white"
+                      : "border-gray-300 text-gray-600 hover:bg-gray-100"
                   }`}
                 >
-                  Add Transaction
+                  Income
+                </button>
+                <button
+                  onClick={() => setType("expense")}
+                  className={`flex-1 rounded-lg border px-4 py-2 font-medium transition ${
+                    type === "expense"
+                      ? "border-red-500 bg-red-600 text-white"
+                      : "border-gray-300 text-gray-600 hover:bg-gray-100"
+                  }`}
+                >
+                  Expense
                 </button>
               </div>
             </div>
-          )}
-        </div>
-     
+
+            {/* add amount */}
+            <div className="mb-6 flex items-center justify-center relative">
+              <IndianRupee className="size-9 text-gray-700 absolute left-1/4 pl-2" />
+              <input
+                type="number"
+                placeholder="0.00"
+                className="w-40 h-9 pl-10  border rounded-lg  pr-0 py-7 text-start tracking-wide text-3xl text-blue-600 focus:border-blue-600 focus:outline-none"
+                value={amount}
+                onChange={(e)=>setAmount(e.target.value)}
+              />
+            </div>
+
+            {/* next btn */}
+            <button
+              onClick={() => setStep(2)}
+              className="w-full rounded-lg bg-blue-600 py-2 text-white hover:bg-blue-700"
+            >
+              Next
+            </button>
+          </div>
+        )}
+
+        {/* details model */}
+        {step === 2 && (
+          <div className="h-3/4 w-full max-w-sm flex flex-col justify-between rounded-xl bg-white p-6 shadow-lg">
+            <div className="mb-4 flex items-start justify-between">
+              <h2 className="text-lg font-semibold">
+                {type === "expense" ? "Add Expense" : "Add Income"}
+              </h2>
+              <button
+                onClick={closeModal}
+                className="text-gray-400 hover:text-gray-600"
+              >
+                ✕
+              </button>
+            </div>
+
+            {/* Form */}
+            <div className="space-y-4">
+              <input
+                type="text"
+                placeholder="Description"
+                className="w-full rounded-lg border px-4 py-2 focus:border-blue-600 focus:outline-none"
+                value={description}
+                onChange={(e)=>setDescription(e.target.value)}
+              />
+
+              {type === "expense" ? (
+                <select 
+                  className="w-full overflow-hidden overflow-y-scroll rounded-lg border px-4 py-2 focus:border-blue-600 focus:outline-none"
+                  value={category}
+                  onChange={(e)=>setCategory(e.target.value)}
+                >
+                  <option value="not selected">Select Category</option>
+                  <option value="food">Food</option>
+                  <option value="bills&utilities">Bills & utilities</option>
+                  <option value="travel">travel</option>
+                  <option value="transportation">Transport</option>
+                  <option value="healthcare">Healthcare</option>
+                  <option value="shopping">Shopping</option>
+                  <option value="education">Education</option>
+                  <option value="entertainment">Entertainment</option>
+                </select>
+              ) : (
+                <input
+                  type="text"
+                  placeholder="Income From"
+                  className="w-full rounded-lg border px-4 py-2 focus:border-blue-600 focus:outline-none"
+                  value={incomeFrom}
+                  onChange={(e)=>setIncomeFrom(e.target.value)}
+                />
+              )}
+
+              <input
+                type="date"
+                className="w-full rounded-lg border px-4 py-2 focus:border-blue-600 focus:outline-none"
+                value={date}
+                onChange={(e)=>setDate(e.target.value)}
+              />
+            </div>
+
+            {/* btns */}
+            <div className="mt-6 flex gap-3">
+              <button
+                onClick={() => setStep(1)}
+                className="flex-1 rounded-lg border py-2 text-gray-600 hover:bg-gray-100"
+              >
+                Back
+              </button>
+              <button
+                className={`flex-1 rounded-lg py-2 text-white ${
+                  type === "expense"
+                    ? "bg-red-600 hover:bg-red-700"
+                    : "bg-green-600 hover:bg-green-700"
+                }`}
+
+                onClick={handleTransaction}
+              >
+                Add Transaction
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
