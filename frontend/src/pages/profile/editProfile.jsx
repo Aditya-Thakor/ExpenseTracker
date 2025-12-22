f18bd324bfaf4632c311ad3c419c7bf8c648e13d
Aditya-Thakor <aditya.github.27x@gmail.com>
2025-12-22 17:36:30 +0530

    added loader

--- frontend/src/pages/profile/editProfile.jsx
+++ frontend/src/pages/profile/editProfile.jsx
@@ -7,26 +7,25 @@ export default function EditProfile() {
   const localUser = JSON.parse(localStorage.getItem("user"));
   const userId = localUser._id;
 
-  const [user,setUser]= useState(null);
-    // useEffect(() => {
-      
-    // }, [user]);
-     useEffect(() => {
-      async function fetchUser() {
-        await fetch("http://localhost:5000/usersdata/")
-          .then((res) => res.json())
-          .then((data) => {
-            let usr = data.find((i) => i._id === userId);
-            // console.log(user);
-            setUser(usr);
-          })
-          .catch((error) => {
-            console.log("error at fetching userdata at dashboard", error);
-          });
-      }
-      fetchUser()
-  
-    }, [userId]);
+  const [user, setUser] = useState(null);
+  // useEffect(() => {
+
+  // }, [user]);
+  useEffect(() => {
+    async function fetchUser() {
+      await fetch("http://localhost:5000/usersdata/")
+        .then((res) => res.json())
+        .then((data) => {
+          let usr = data.find((i) => i._id === userId);
+          // console.log(user);
+          setUser(usr);
+        })
+        .catch((error) => {
+          console.log("error at fetching userdata at dashboard", error);
+        });
+    }
+    fetchUser();
+  }, [userId]);
 
   const fileInputRef = useRef(null);
   const [preview, setPreview] = useState("");
@@ -40,8 +39,12 @@ export default function EditProfile() {
   const [at, setAt] = useState(user?.address?.at || "didn't added");
   const [city, setCity] = useState(user?.address?.city || "didn't added");
   const [state, setState] = useState(user?.address?.state || "didn't added");
-  const [country, setCountry] = useState(user?.address?.country || "didn't added");
-  const [pincode, setPincode] = useState(user?.address?.pincode || "didn't added");
+  const [country, setCountry] = useState(
+    user?.address?.country || "didn't added"
+  );
+  const [pincode, setPincode] = useState(
+    user?.address?.pincode || "didn't added"
+  );
 
   const handleFileref = () => {
     fileInputRef.current.click();
@@ -108,8 +111,224 @@ export default function EditProfile() {
 
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div className="h-full w-full flex flex-col gap-5 p-5">
-        <h1
+      {user ? (
+        <div className="h-full w-full flex flex-col gap-5 p-5">
+          <h1
+            className="flex items-center gap-1 cursor-pointer text-gray-500 font-medium hover:text-gray-800"
+            onClick={() => navigate("/profile")}
+          >
+            <span>
+              <ChevronLeft className="size-5" />
+            </span>
+            <span>Back</span>
+          </h1>
+          <div className="h-[90%] w-full flex flex-col gap-3 rounded-xl bg-white p-3">
+            <div className="h-10 w-full flex justify-between items-center">
+              <span className="text-xl font-semibold text-gray-800">
+                Edit profile
+              </span>
+              <button
+                className="px-4 py-0.5 text-center bg-green-500 text-white rounded-lg font-medium"
+                onClick={editUser}
+              >
+                Save
+              </button>
+            </div>
+
+            <div className="h-40 w-full flex gap-3 ">
+              {/* change profile img */}
+              <div className="h-full w-40 bg-transparent flex justify-center relative rounded-2xl overflow-hidden ">
+                <img
+                  src={
+                    preview
+                      ? preview
+                      : user?.pfp
+                      ? `http://localhost:5000/profileImages/${user.pfp}`
+                      : i.pfp1
+                  }
+                  alt="pfp"
+                  className="h-full w-full object-cover relative"
+                />
+                <input
+                  type="file"
+                  accept="image/*"
+                  ref={fileInputRef}
+                  onChange={handleFile}
+                  className="hidden"
+                />
+                <button
+                  onClick={handleFileref}
+                  className="w-full px-2 py-2 bg-blue-500/80 hover:bg-blue-500 font-medium text-white absolute bottom-0 "
+                >
+                  Change profile
+                </button>
+              </div>
+
+              <div className="h-full w-[calc(100%-10rem)] grid grid-cols-2  gap-4">
+                <div className="h-16 ">
+                  <label
+                    htmlFor="displayname"
+                    className="flex flex-col gap-1 text-gray-400"
+                  >
+                    <span>Change Full name : </span>
+                    <input
+                      placeholder={user?.fullname}
+                      value={fullname}
+                      onChange={(e) => setFullName(e.target.value)}
+                      type="text"
+                      className="rounded-md p-2 border"
+                    />
+                  </label>
+                </div>
+                <div className="h-16 ">
+                  <label
+                    htmlFor="usename"
+                    className="flex flex-col gap-1 text-gray-400"
+                  >
+                    <span>Change username : </span>
+                    <input
+                      value={username}
+                      placeholder={user?.username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      type="text"
+                      className="rounded-md p-2 border"
+                    />
+                  </label>
+                </div>
+                <div className="h-16 ">
+                  <label
+                    htmlFor="email"
+                    className="flex flex-col gap-1 text-gray-400"
+                  >
+                    <span>Change email address : </span>
+                    <input
+                      placeholder={user?.email}
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      type="text"
+                      className="rounded-md p-2 border"
+                    />
+                  </label>
+                </div>
+                <div className="h-16 ">
+                  <label
+                    htmlFor="role"
+                    className="flex flex-col gap-1 text-gray-400"
+                  >
+                    <span>Change role : </span>
+                    <input
+                      placeholder={user?.role}
+                      value={role}
+                      onChange={(e) => setRole(e.target.value)}
+                      type="text"
+                      className="rounded-md p-2 border"
+                    />
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <div className="h-[calc(100%-12.5rem)] flex flex-col gap-5 ">
+              <div>
+                <label htmlFor="address" className="flex flex-col">
+                  <span className="text-gray-400">Change Address :</span>
+                  <textarea
+                    id="adrress"
+                    rows={2}
+                    className="border text-gray-400 rounded-md"
+                    value={at}
+                    onChange={(e) => setAt(e.target.value)}
+                  />
+                </label>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <div className="h-16 ">
+                  <label
+                    htmlFor="city"
+                    className="flex flex-col gap-1 text-gray-400"
+                  >
+                    <span>Change city name : </span>
+                    <input
+                      id="city"
+                      type="text"
+                      value={city}
+                      onChange={(e) => setCity(e.target.value)}
+                      className="rounded-md p-2 border"
+                    />
+                  </label>
+                </div>
+                <div className="h-16 ">
+                  <label
+                    htmlFor="state"
+                    className="flex flex-col gap-1 text-gray-400"
+                  >
+                    <span>Change state : </span>
+                    <input
+                      id="state"
+                      type="text"
+                      value={state}
+                      onChange={(e) => setState(e.target.value)}
+                      className="rounded-md p-2 border"
+                    />
+                  </label>
+                </div>
+                <div className="h-16 ">
+                  <label
+                    htmlFor="country"
+                    className="flex flex-col gap-1 text-gray-400"
+                  >
+                    <span>Change country : </span>
+                    <input
+                      id="country"
+                      type="text"
+                      value={country}
+                      onChange={(e) => setCountry(e.target.value)}
+                      className="rounded-md p-2 border"
+                    />
+                  </label>
+                </div>
+                <div className="h-16 ">
+                  <label
+                    htmlFor="pincode"
+                    className="flex flex-col gap-1 text-gray-400"
+                  >
+                    <span>Change pincode : </span>
+                    <input
+                      id="pincode"
+                      type="text"
+                      value={pincode}
+                      onChange={(e) => setPincode(e.target.value)}
+                      className="rounded-md p-2 border"
+                    />
+                  </label>
+                </div>
+              </div>
+              <button
+                className="bg-green-500/80 hover:bg-green-500 text-white font-medium py-1 rounded-lg"
+                onClick={editUser}
+              >
+                Save Changes
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        // loader
+        <div className="h-full w-full flex justify-center items-center">
+          <span className="h-40 w-40 flex justify-center items-center text-5xl text-blue-500 rounded-full ">
+            <span>Loading</span> 
+            <span className="animate-pulse">.</span>
+            <span className="animate-pulse">.</span>
+            <span className="animate-pulse">.</span>
+          </span>
+          
+        </div>
+      )}
+
+      {/* code :  */}
+
+      {/* <div className="h-full w-full flex flex-col gap-5 p-5"> */}
+      {/* <h1
           className="flex items-center gap-1 cursor-pointer text-gray-500 font-medium hover:text-gray-800"
           onClick={() => navigate("/profile")}
         >
@@ -117,9 +336,9 @@ export default function EditProfile() {
             <ChevronLeft className="size-5" />
           </span>
           <span>Back</span>
-        </h1>
-        <div className="h-[90%] w-full flex flex-col gap-3 rounded-xl bg-white p-3">
-          <div className="h-10 w-full flex justify-between items-center">
+        </h1> */}
+      {/* <div className="h-[90%] w-full flex flex-col gap-3 rounded-xl bg-white p-3"> */}
+      {/* <div className="h-10 w-full flex justify-between items-center">
             <span className="text-xl font-semibold text-gray-800">
               Edit profile
             </span>
@@ -129,11 +348,11 @@ export default function EditProfile() {
             >
               Save
             </button>
-          </div>
+          </div> */}
 
-          <div className="h-40 w-full flex gap-3 ">
-            {/* change profile img */}
-            <div className="h-full w-40 bg-transparent flex justify-center relative rounded-2xl overflow-hidden ">
+      {/* <div className="h-40 w-full flex gap-3 "> */}
+      {/* change profile img */}
+      {/* <div className="h-full w-40 bg-transparent flex justify-center relative rounded-2xl overflow-hidden ">
               <img
                 src={
                   preview
@@ -158,9 +377,9 @@ export default function EditProfile() {
               >
                 Change profile
               </button>
-            </div>
+            </div> */}
 
-            <div className="h-full w-[calc(100%-10rem)] grid grid-cols-2  gap-4">
+      {/* <div className="h-full w-[calc(100%-10rem)] grid grid-cols-2  gap-4">
               <div className="h-16 ">
                 <label
                   htmlFor="displayname"
@@ -222,9 +441,9 @@ export default function EditProfile() {
                 </label>
               </div>
             </div>
-          </div>
+          </div> */}
 
-          <div className="h-[calc(100%-12.5rem)] flex flex-col gap-5 ">
+      {/* <div className="h-[calc(100%-12.5rem)] flex flex-col gap-5 ">
             <div>
               <label htmlFor="address" className="flex flex-col">
                 <span className="text-gray-400">Change Address :</span>
@@ -305,9 +524,9 @@ export default function EditProfile() {
             >
               Save Changes
             </button>
-          </div>
-        </div>
-      </div>
+          </div> */}
+      {/* </div>
+      </div> */}
     </div>
   );
 }
