d43b538361f5fca8244cd87f2b242e2d23513935
Aditya-Thakor <aditya.github.27x@gmail.com>
2025-12-23 15:37:39 +0530

    created transaction context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar/Navbar' 
+import TransactionContextProvider from './context/Provider'
 
 function App() { 
 
   return (
-    <div className='flex bg-[#F5F8FF]'>
-       <Navbar/>
-       <Outlet/>
-    </div>
+    // <TransactionContextProvider>
+      <div className='flex bg-[#F5F8FF]'>
+        <Navbar/>
+        <Outlet/>
+      </div>
+    // </TransactionContextProvider>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,13 @@ import ETRoutes from './routes/Routes.jsx'
 import SignIn from './pages/signup&signin/SignIn.jsx'  //demo removed by adi-codespace
 import FancyChart from './components/charts/Demo2.jsx'
 import AddTransaction from './components/addTransactions/AddTransaction.jsx'
+import TransactionContextProvider from './context/Provider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ETRoutes/> 
+   
+      <ETRoutes/> 
+   
     {/* <AddTransaction/> */}
-  </StrictMode>,
+  </StrictMode>
 )
