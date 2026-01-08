2ae266de1440e7e30418e8029611f26d52859abf
aditya thakor <aditya.github.27x@gmail.com>
2026-01-08 17:20:13 +0530

    added dynamic data & fixed filter

--- frontend/src/pages/analytics/Report.jsx
+++ frontend/src/pages/analytics/Report.jsx
@@ -1,179 +1,204 @@
-import { FileSearchCorner, MoveRight } from "lucide-react";
+import { Download, FileSearchCorner, MoveRight, RotateCw } from "lucide-react";
 import TransactionCard from "../../components/Income-expense-Card/TransactionCard";
 import TransactionContext from "../../context/TransactionContext";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Report() {  
-    const usr=JSON.parse(localStorage.getItem("user")) ;
-    const uid = usr._id
-    // console.log("UID-",uid);
-    const [user,setUser]=useState(null);
-    const [tr,setTr]=useState([]);  
+export default function Report() {
+  const { transactions, totalExpense, totalIncome } =
+    useContext(TransactionContext);
+  const [tr, setTr] = useState([]);
+  const [exTotal, setExTotal] = useState(0);
+  const [inTotal, setInTotal] = useState(0);
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
 
-useEffect(()=>{
- fetch("http://localhost:5000/usersdata/")
-        .then((res) => res.json())
-        .then((data) => {
-          let usr = data.find((i) => i._id === uid);
-          // console.log("usr-", usr);
-          setUser(usr);
-        })
-        .catch((error) => {
-          console.log("error at fetching userdata at dashboard", error);
-        });
-},[uid])
-    
-  const transactions = [
-    {
-      id: 1,
-      type: "income",
-      tag: "Monthly Salary",
-      category: "Salary",
-      amount: 50000,
-      date: "2026-01-01",
-    },
-    {
-      id: 2,
-      type: "expense",
-      tag: "House Rent",
-      category: "Rent",
-      amount: 12000,
-      date: "2026-01-02",
-    },
-    {
-      id: 3,
-      type: "expense",
-      tag: "Grocery Shopping",
-      category: "Groceries",
-      amount: 3500,
-      date: "2026-01-03",
-    },
-    {
-      id: 4,
-      type: "income",
-      tag: "Client Project",
-      category: "Freelance",
-      amount: 8000,
-      date: "2026-01-05",
-    },
-    {
-      id: 5,
-      type: "expense",
-      tag: "Power Bill",
-      category: "Electricity Bill",
-      amount: 1800,
-      date: "2026-01-06",
-    },
-    {
-      id: 6,
-      type: "expense",
-      tag: "WiFi Recharge",
-      category: "Internet",
-      amount: 1200,
-      date: "2026-01-07",
-    },
-    {
-      id: 7,
-      type: "income",
-      tag: "Year-End Bonus",
-      category: "Bonus",
-      amount: 5000,
-      date: "2026-01-10",
-    },
-    {
-      id: 8,
-      type: "expense",
-      tag: "Daily Commute",
-      category: "Transport",
-      amount: 900,
-      date: "2026-01-11",
-    },
-    {
-      id: 9,
-      type: "expense",
-      tag: "Movie Night",
-      category: "Entertainment",
-      amount: 2500,
-      date: "2026-01-13",
-    },
-    {
-      id: 10,
-      type: "income",
-      tag: "Stock Profit",
-      category: "Investment Return",
-      amount: 3000,
-      date: "2026-01-15",
-    },
-  ];
-  
-    const th=[ "Date","Title","Class","Expense","Income" ]
+  const [instruction, setInstruction] = useState(true);
+  const [noData, setNoData] = useState(false);
+
+  const handleStatement = () => {
+    if (!from && !to) return setInstruction(true);
+    if (from > to && to.length != 0) return setTr([]) || setNoData(true); //setNoData(true) || setInstruction(false)
+    // console.log(from,"->",to);
+    if (from && !to) {
+      setInstruction(false);
+      setTr([]);
+      const statement = transactions.filter(
+        (t) =>
+          new Date(t.date) <= new Date() && new Date(t.date) >= new Date(from)
+      );
+      // console.log("statement");
+      // console.log(statement.filter((t)=>t.type=="expense"));
+      if (statement.length === 0) {
+        setNoData(true);
+        return;
+      }
+      setNoData(false);
+      return setTr(statement);
+    }
+    if (!from && to) {
+      setInstruction(false);
+      setTr([]);
+      const statement = transactions.filter(
+        (t) => new Date(t.date) <= new Date(to)
+      );
+      // console.log(statement);
+      if (statement.length === 0) {
+        setNoData(true);
+        return;
+      }
+      setNoData(false);
+      return setTr(statement);
+    }
+    if (from && to) {
+      setInstruction(false);
+      setTr([]);
+      const statement = transactions.filter(
+        (t) =>
+          new Date(t.date) <= new Date(to) && new Date(t.date) >= new Date(from)
+      );
+      // console.log("statement");
+      // console.log(statement.filter((t)=>t.type=="income"));
+      if (statement.length === 0) {
+        setNoData(true);
+        return;
+      }
+      setNoData(false);
+      return setTr(statement);
+    }
+  };
+
+  useMemo(() => {
+    const ext = tr
+      ?.filter((t) => t.type == "expense")
+      .reduce((sum, t) => {
+        return sum + t.amount;
+      }, 0);
+    setExTotal(ext);
+    const int = tr
+      ?.filter((t) => t.type == "income")
+      .reduce((sum, t) => {
+        return sum + t.amount;
+      }, 0);
+    setInTotal(int);
+    // console.log("extt-",ext);
+  }, [tr, from, to]);
+
+  // console.log("from", from); // yyyy-mm-dd
+
+  const th = ["Date", "Title", "Class", "Expense", "Income"];
   return (
-    <div className="h-auto lg:h-screen w-full flex flex-col items-center px-10 gap-10 bg-gradient-to-b from-blue-200  to-cyan-50 ">
-      <div className="lg:h-32 w-full flex justify-center items-center gap-3 bg-white text-neutral-600 lg:mt-3 rounded-xl shadow-sm px-4">
+    <div
+      className={`${
+        tr.length ? "h-auto" : "h-screen"
+      }  w-full flex flex-col items-center relative px-10 gap-10 bg-gradient-to-b from-blue-200  to-cyan-50 `}
+    >
+      <div className="h-28 w-full flex justify-center items-center gap-3 bg-white text-neutral-600 lg:mt-3 rounded-xl shadow-sm px-4"> 
         <label htmlFor="fromDate" className=" border px-5 py-3 rounded-lg ">
           <span className="font-semibold">From : </span>
           <input
             id="fromDate"
             type="date"
             className="ml-2 text-cyan-600 font-medium focus:outline-none"
+            onChange={(e) => setFrom(e.target.value)}
           />
         </label>
         <span>
           <MoveRight />
         </span>
-        <label htmlFor="fromDate" className=" border px-5 py-3 rounded-lg">
+        <label htmlFor="toDate" className=" border px-5 py-3 rounded-lg">
           <span className="font-semibold">To : </span>
           <input
-            id="fromDate"
+            id="toDate"
             type="date"
             className="ml-2 text-cyan-600 font-medium focus:outline-none"
+            onChange={(e) => setTo(e.target.value)}
           />
         </label>
-        <span className="w-16 py-2 rounded-lg flex justify-center items-center cursor-pointer text-white/50 bg-gradient-to-l from-blue-500/50 to-cyan-500/50 hover:bg-gradient-to-b hover:from-cyan-500 hover:to-blue-500 hover:text-white transition-all ease-in-out  ">
+        <span
+          className="w-16 py-2 rounded-lg flex justify-center items-center cursor-pointer text-white/50 bg-gradient-to-l from-blue-500/50 to-cyan-500/50 hover:bg-gradient-to-b hover:from-cyan-500 hover:to-blue-500 hover:text-white transition-all ease-in-out  "
+          onClick={handleStatement}
+        >
           <FileSearchCorner className="size-8" />
         </span>
+        {tr.length? <span className="text-neutral-400 hover:text-neutral-700 hover:transform hover:rotate-180 transition-all ease-linear duration-500 cursor-pointer ">
+          <RotateCw onClick={() => window.location.reload()} />
+        </span>:'' }
+        
+        {tr.length ? (
+          <button className="flex items-center absolute right-16 h-min text-gray-500 bg-blue-200  rounded-lg py-3 px-5 gap-2 shadow-sm hover:text-gray-900  hover:shadow-lg">
+            <span className="">
+              <Download className="size-5" />
+            </span>
+            <span className="text-sm ">Export report</span>
+          </button>
+        ) : (
+          ""
+        )}
       </div>
-      <div className="h-auto w-full flex flex-col gap-3 ">
+      <div
+        className={`md:min-h-[calc(100vh-10.5rem)] w-full  flex-col gap-20 ${
+          tr.length ? "flex" : "hidden"
+        } `}
+      >
         <table className="">
           <thead className="mb-4">
             <tr className="border-b-2 border-white">
-                {
-                    th.map((t,ind)=>(
-                        <th key={ind} className="text-start text-lg text-neutral-700">{t}</th>
-                    ))
-                } 
+              {th.map((t, ind) => (
+                <th key={ind} className="text-start text-lg text-neutral-700">
+                  {t}
+                </th>
+              ))}
             </tr>
           </thead>
-          <tbody className="">
-            {transactions?.map((item) => (
-              <tr key={item.id} className="text-sm text-neutral-600">
-                <td>{item.date}</td>
-                <td>{item.tag}</td>
-                <td>{item.category}</td>
-                <td>{item.type=="expense"?item.amount:0}</td>
-                <td>{item.type=="income"?item.amount:0}</td>
+          <tbody className={`h-auto overflow-y-scroll `}>
+            {tr?.map((t) => (
+              <tr key={t._id} className="text-sm text-neutral-600">
+                <td>{t.date.slice(0, 10)}</td>
+                <td>{t.description}</td>
+                <td>{t.type == "expense" ? t.category : t.incomeFrom}</td>
+                <td>{t.type == "expense" ? t.amount : 0}</td>
+                <td>{t.type == "income" ? t.amount : 0}</td>
               </tr>
             ))}
           </tbody>
         </table>
-        <div className="h-auto max-w-xs bg-white text-neutral-800 font-lato p-5">
-            <h3 className="font-semibold text-xl mb-3">Summery</h3>
-            <div className="h-full w-full flex flex-col   text-sm"> 
-                <div className="flex items-center justify-between">
-                    <span>Total Income :- </span>
-                    <span className="text-xl">Rs. 15151</span>
-                </div>
-                <div className="flex items-center justify-between border-b border-dashed border-neutral-700 ">
-                    <span>Total Expense :- </span>
-                    <span className="text-xl">Rs. 15151</span>
-                </div>
-                <div className="flex items-center justify-between">
-                    <span>Net Balance :- </span>
-                    <span className="text-xl">Rs. 15151</span>
-                </div>
+        <div className="h-auto max-w-lg w-1/2 ml-auto bg-white text-neutral-800 font-lato p-5 mb-10 border border-dashed border-blue-300 shadow-lg">
+          <h3 className="font-semibold text-xl mb-3">Summery</h3>
+          <div className="h-full w-full flex flex-col   text-sm">
+            <div className="flex items-center justify-between">
+              <span>Total Income :- </span>
+              <span className="text-xl">Rs. {inTotal}</span>
             </div>
+            <div className="flex items-center justify-between border-b border-dashed border-neutral-700 ">
+              <span>Total Expense :- </span>
+              <span className="text-xl">Rs. {exTotal}</span>
+            </div>
+            <div className="flex items-center justify-between">
+              <span>Net Balance :- </span>
+              <span className="text-xl">Rs. {inTotal - exTotal}</span>
+            </div>
+          </div>
         </div>
       </div>
+
+      {instruction == true ? (
+        <div className="h-auto w-3/4 text-center text-xl font-lato ">
+          <h1 className="animate-pulse text-neutral-700">
+            Select a date<span className="text-xs">(s)</span> to view your
+            transactions.
+          </h1>
+        </div>
+      ) : (
+        ""
+      )}
+      {noData ? (
+        <div className="h-40 w-1/2 flex justify-center items-center text-neutral-600 text-center text-xl font-lato bg-white/40 rounded-xl shadow-sm shadow-slate-400/50 ">
+          <h1>No transactions found. Try selecting a different date.</h1>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
