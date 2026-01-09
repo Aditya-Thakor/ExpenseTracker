cec7a745f5047b7077a880faaaf31db832b0d2f9
aditya thakor <aditya.github.27x@gmail.com>
2026-01-09 17:16:34 +0530

    add code for create xls file

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const { error, log } = require('console')
+const fs = require('fs')
+
+fs.writeFile('et.xls','React js \tNode js\nReact Demo\tNode demo', (error)=>{
+    if(error) return console.log("Error at creating the file!!",error);
+    console.log('File created Successfully!!!');        
+})
+
+fs.readFile('et.xls','utf-8',(error,data)=>{
+    console.log(data);    
+})
+
+data = fs.readFileSync("et.xls","utf-8");
+console.log(data);
+
+fs.readFile("et.xls","utf-8",(error,data)=>{
+    console.log(data);    
+})
