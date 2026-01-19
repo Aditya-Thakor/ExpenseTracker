c3a0d10c307d9b41d467975c48a9368e720e06cc
aditya thakor <aditya.github.27x@gmail.com>
2026-01-19 16:34:39 +0530

    dotenv bug fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,8 @@
-const { error, log } = require('console')
 require('dotenv').config();
-
 const port = process.env.PORT;
-console.log(`Server running on port ${port}`);
+const { error, log } = require('console')
 
-// const fs = require('fs')
 
-// fs.writeFile('et.xls','React js \tNode js\nReact Demo\tNode demo', (error)=>{
-//     if(error) return console.log("Error at creating the file!!",error);
-//     console.log('File created Successfully!!!');        
-// })
 
-// fs.readFile('et.xls','utf-8',(error,data)=>{
-//     console.log(data);    
-// })
+console.log(`Server running on port ${port}`); 
 
-// data = fs.readFileSync("et.xls","utf-8");
-// console.log(data);
-
-// fs.readFile("et.xls","utf-8",(error,data)=>{
-//     console.log(data);    
-// })
