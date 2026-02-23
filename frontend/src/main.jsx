1ef608e189e087845655f7e5aa35b5db6fe11a50
aditya thakor <aditya.github.27x@gmail.com>
2026-02-23 18:15:34 +0530

    wrapedup new transaction api.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import ETRoutes from './routes/Routes.jsx'
+import { TransactionProvider } from './context/transactionContext/TransactionContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   
+   <TransactionProvider>
       <ETRoutes/> 
+   </TransactionProvider>
    
   </StrictMode>
 )
